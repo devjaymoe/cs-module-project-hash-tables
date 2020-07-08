3159da48be7317201354a1c491eb78e245ba43ec
Devin_Moore <d.j.moore209@gmail.com>
2020-07-08 17:52:08 -0400

    completed no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,19 @@
 def no_dups(s):
     # Your code here
+    cache = {}
+    str_arr = s.split()
 
+    for x in str_arr:
 
+        if x not in cache:
+            cache[x] = x
+
+    a = ""
+
+    for x in cache:
+        a += cache[x] + " "
+    
+    return a.rstrip()
 
 if __name__ == "__main__":
     print(no_dups(""))
