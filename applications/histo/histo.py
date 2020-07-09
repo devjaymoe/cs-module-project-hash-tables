e70c4c93474ba91570bd03d3eaf21639f3eb855a
Devin_Moore <d.j.moore209@gmail.com>
2020-07-09 18:54:45 -0400

    histo mostly done

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,35 @@
 # Your code here
 
+# Read in all the words in one go
+with open("robin.txt") as f:
+    words = f.read()
+
+# TODO: analyze which words can follow other words
+# Your code here
+
+bad_chars = ["\"", ":", ";", ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+words = words.lower()
+
+for x in bad_chars:
+
+    words = words.replace(x, '')
+
+words_arr = words.split()
+cache = {}
+
+for x in words_arr:
+
+    if x in cache:
+        cache[x] += 1
+    else:
+        cache[x] = 1
+
+if __name__ == "__main__":
+    while True:
+        text = input("Enter text: ")
+
+        number_of_times_appeared = cache[text]
+
+        print(text, end="      ")
+        print("#" * number_of_times_appeared)
